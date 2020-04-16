fcd8eef79715b5bd8aaa496b92f24d069db5f281
IsoaSFlus <me@isoasflus.me>
2020-04-16 20:25:28 +0800

    Update

--- lineage.mk
+++ lineage.mk
@@ -2,4 +2,5 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
 $(call inherit-product, vendor/Lawnchair/config.mk)
+$(call inherit-product, external/pingfang-fonts/fonts.mk)
 
