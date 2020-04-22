9e852396c39cd6a106170bdc1faee8f766f188b5
IsoaSFlus <me@isoasflus.me>
2020-04-22 11:34:20 +0800

    update

--- lineage.mk
+++ lineage.mk
@@ -6,8 +6,8 @@ $(call inherit-product, external/pingfang-fonts/fonts.mk)
 
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/phh/treble/rootdir/emui,system/emui)
 
-$(shell mkdir -p ${OUT}"/system")
-$(shell cp -rf device/phh/treble/rootdir/emui ${OUT)}"/system/emui")
+$(shell mkdir -p "${OUT_DIR}/system")
+$(shell cp -rf device/phh/treble/rootdir/emui "${OUT_DIR}/system/emui")
 
 PRODUCT_PACKAGES += \
 	init.victoria.rc
