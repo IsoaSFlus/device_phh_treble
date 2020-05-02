5240bb72255634e853bb2431b845f191bdb732c6
IsoaSFlus <me@isoasflus.me>
2020-05-02 21:50:37 +0800

    update

--- gapps.mk
+++ gapps.mk
@@ -12,12 +12,13 @@ WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_DIALER_OVERRIDES := true
 PRODUCT_PACKAGES += \
        Chrome \
        CalculatorGoogle \
        PrebuiltDeskClockGoogle \
        CalendarGooglePrebuilt \
-       LatinImeGoogle \
        phh-overrides
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
